ed89f7a18f8a0ec10dc1444fe867c9561d524913
Mounika <mounika27boddu@gmail.com>
2023-12-20 11:14:20 -0600

    test

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import config from './aws-exports';
 import { Amplify } from 'aws-amplify';
 import { BrowserRouter } from 'react-router-dom'
 Amplify.configure(config);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
