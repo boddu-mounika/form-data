509474bb198a5f9eafa39a995b4d4d10a2042b33
Mounika <mounika27boddu@gmail.com>
2023-12-20 11:03:51 -0600

    added all changes i know

--- src/index.js
+++ src/index.js
@@ -7,7 +7,6 @@ import config from './aws-exports';
 import { Amplify } from 'aws-amplify';
 import { BrowserRouter } from 'react-router-dom'
 Amplify.configure(config);
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <BrowserRouter>
